847775d5d3ef294e3bef4393a15559d7e8136f15
Allen brady <abrady@cirruspath.com>
2014-05-03 17:18:13 -0500

    Fixed the accidental revert, made this new branch

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get '/plans', to: 'welcome#plans'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
